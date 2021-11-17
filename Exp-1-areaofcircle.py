7b6493e846120359569865274c7f50db3f705089
GOKUL N KUMAR <gokulnirmalkumar@gmail.com>
2021-11-17 13:46:30 +0530

    Experiment 1
    Area of circle

---
+++ Exp-1-areaofcircle.py
@@ -0,0 +1,7 @@
+import math
+radius=float(input("Enter radius of circle:"))
+pi=math.pi
+area=pi*radius*radius
+perimeter=2*pi*radius
+print("Area of cicle is:",area)
+print("Perimeter of circle is:",perimeter)
